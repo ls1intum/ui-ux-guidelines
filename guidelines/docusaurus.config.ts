b4d956c36d9454ede3fec0a77094308d763dadc7
Florian Glombik <florian.glombik@tum.de>
2025-10-08 14:40:02 +0200

    Add github deployment info

--- guidelines/docusaurus.config.ts
+++ guidelines/docusaurus.config.ts
@@ -24,8 +24,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/username.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ls1intum', // Usually your GitHub org/username.
+  projectName: 'ui-ux-guidelines', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
