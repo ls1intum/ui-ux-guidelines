e9b13227f94d122d9142ac6538c6a8b60e3cb876
Florian Glombik <florian.glombik@tum.de>
2025-10-09 22:37:35 +0200

    Add production url

--- guidelines/docusaurus.config.ts
+++ guidelines/docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ls1intum.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ui-ux-guidelines/',
